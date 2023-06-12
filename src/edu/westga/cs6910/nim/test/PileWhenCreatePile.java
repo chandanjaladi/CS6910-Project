f3421c3ef038dfd462eb4905eafdd5d737b92b80
Chandan Jaladi <cj00216@my.westga.edu>
2023-06-12 11:38:32 -0400

    Added a test case for the constructor of the pile class.

---
+++ src/edu/westga/cs6910/nim/test/PileWhenCreatePile.java
@@ -0,0 +1,17 @@
+package edu.westga.cs6910.nim.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.nim.model.Pile;
+
+class PileWhenCreatePile {
+
+	@Test
+	public void testShouldPRoducePileWith1Stick() {
+		Pile oneStickPile = new Pile(1);
+		assertEquals("Pile size: 1",oneStickPile.toString());
+	}
+
+}
