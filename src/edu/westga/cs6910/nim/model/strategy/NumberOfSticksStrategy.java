92a2135dbb4fc9ff7687d624c012dc7f5bb906b8
Chandan Jaladi <cj00216@my.westga.edu>
2023-06-20 02:04:54 -0400

    Added a new interface to the application

--- src/edu/westga/cs6910/nim/model/strategy/NumberOfSticksStrategy.java
+++ src/edu/westga/cs6910/nim/model/strategy/NumberOfSticksStrategy.java
@@ -1,6 +1,10 @@
 package edu.westga.cs6910.nim.model.strategy;
 
 public interface NumberOfSticksStrategy {
-	
+	/**
+	 * 
+	 * @param sizeOfPile
+	 * @return
+	 */
 	int howManySticks(int sizeOfPile);
 }
